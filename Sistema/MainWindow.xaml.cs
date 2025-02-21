ca2d34f02887b0746617d81118a3203f71441e6b
Douglas <douglasmodolo@outlook.com.br>
2025-02-21 20:03:36 -0300

    Add project files.

---
+++ Sistema/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Sistema;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+	private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+	{
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            this.DragMove();
+        }
+	}
+}
